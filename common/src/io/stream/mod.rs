47f3c1c0c338ec88bf10f9b445041907f8047fad
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2025-12-25 09:58:16 +0000

    clippy mv stream.rs to zisk_stream.rs

--- common/src/io/stream/mod.rs
+++ common/src/io/stream/mod.rs
@@ -2,9 +2,9 @@ mod file;
 mod memory;
 mod null;
 mod quic;
-mod stream;
 mod stream_reader;
 mod stream_writer;
+mod zisk_stream;
 
 #[cfg(unix)]
 mod unix_socket;
@@ -13,9 +13,9 @@ pub use file::{FileStreamReader, FileStreamWriter};
 pub use memory::MemoryStreamReader;
 pub use null::NullStreamReader;
 pub use quic::{QuicStreamReader, QuicStreamWriter};
-pub use stream::*;
 pub use stream_reader::*;
 pub use stream_writer::*;
+pub use zisk_stream::*;
 
 #[cfg(unix)]
 pub use unix_socket::{UnixSocketStreamReader, UnixSocketStreamWriter};
