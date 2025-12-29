a80ed5fe8be0fb4681b5908c3634de9044dbb292
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2025-12-29 06:44:35 +0000

    move stdin related files to a specific folder

--- common/src/io/mod.rs
+++ common/src/io/mod.rs
@@ -1,11 +1,5 @@
-mod file_stdin;
-mod memory_stdin;
-mod null_stdin;
+mod stdin;
 mod stream;
-mod zisk_stdin;
 
-pub use file_stdin::*;
-pub use memory_stdin::*;
-pub use null_stdin::*;
+pub use stdin::*;
 pub use stream::*;
-pub use zisk_stdin::*;
