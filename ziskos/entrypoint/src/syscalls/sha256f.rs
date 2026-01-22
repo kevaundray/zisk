72a386d66e9bbf41c71c2f2c804578944d4fc27b
fractasy <fractasy@gmail.com>
2026-01-22 12:12:07 +0100

    Implement sha256f in zisklib syscall with hints

--- ziskos/entrypoint/src/syscalls/sha256f.rs
+++ ziskos/entrypoint/src/syscalls/sha256f.rs
@@ -6,6 +6,13 @@ use core::arch::asm;
 #[cfg(all(target_os = "zkvm", target_vendor = "zisk"))]
 use crate::ziskos_syscall;
 
+#[cfg(not(all(target_os = "zkvm", target_vendor = "zisk")))]
+use sha2::compress256;
+
+#[cfg(not(all(target_os = "zkvm", target_vendor = "zisk")))]
+#[allow(deprecated)]
+use sha2::digest::generic_array::{typenum::U64, GenericArray};
+
 #[derive(Debug)]
 #[repr(C)]
 pub struct SyscallSha256Params<'a> {
@@ -35,5 +42,21 @@ pub extern "C" fn syscall_sha256_f(
     #[cfg(all(target_os = "zkvm", target_vendor = "zisk"))]
     ziskos_syscall!(0x805, params);
     #[cfg(not(all(target_os = "zkvm", target_vendor = "zisk")))]
-    unreachable!()
+    {
+        sha256f(params.state, params.input);
+
+        #[cfg(feature = "hints")]
+        {
+            hints.extend_from_slice(params.state);
+        }
+    }
+}
+
+#[cfg(not(all(target_os = "zkvm", target_vendor = "zisk")))]
+#[allow(deprecated)]
+fn sha256f(state: &mut [u64; 4], input: &[u64; 8]) {
+    let state_u32: &mut [u32; 8] = unsafe { &mut *(state.as_mut_ptr() as *mut [u32; 8]) };
+    let input_u8: &[GenericArray<u8, U64>; 1] =
+        unsafe { &*(input.as_ptr() as *const [GenericArray<u8, U64>; 1]) };
+    compress256(state_u32, input_u8);
 }
