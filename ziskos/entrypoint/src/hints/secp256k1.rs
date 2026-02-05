b8327ec8e380e59c2180de069ae56455bd229b7d
Fran Segovia <agnusmor@gmail.com>
2026-02-05 19:09:42 +0000

    Fix ECDSA verify hint ID

--- ziskos/entrypoint/src/hints/secp256k1.rs
+++ ziskos/entrypoint/src/hints/secp256k1.rs
@@ -1,7 +1,7 @@
 use crate::hints::macros::define_hint;
 
 const SECP256K1_ECDSA_RECOVER_HINT_ID: u32 = 0x0300;
-const SECP256K1_ECDSA_VERIFY_SIGNATURE_HINT_ID: u32 = 0x0301;
+const SECP256K1_ECDSA_VERIFY_HINT_ID: u32 = 0x0302;
 
 define_hint! {
     secp256k1_ecdsa_address_recover => {
@@ -13,7 +13,7 @@ define_hint! {
 
 define_hint! {
     secp256k1_ecdsa_verify_and_address_recover => {
-        hint_id: SECP256K1_ECDSA_VERIFY_SIGNATURE_HINT_ID,
+        hint_id: SECP256K1_ECDSA_VERIFY_HINT_ID,
         params: (sig: 64, msg: 32, pk: 64),
         is_result: false,
     }
