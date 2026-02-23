2867fa878243d538885a47abb086a6368cd98ab4
Xavier Pinsach <10213118+xavi-pinsach@users.noreply.github.com>
2026-02-23 17:45:44 +0100

    fix macOS

--- executor/src/emu_asm_stub.rs
+++ executor/src/emu_asm_stub.rs
@@ -7,11 +7,9 @@ use crate::{DeviceMetricsList, NestedDeviceMetricsList, StaticSMBundle};
 use asm_runner::AsmRunnerMO;
 
 use crate::AsmResources;
-use anyhow::Result;
 use fields::PrimeField64;
 use proofman_common::ProofCtx;
 use sm_rom::RomSM;
-use zisk_common::io::StreamSource;
 use zisk_common::{io::ZiskStdin, EmuTrace, ExecutorStatsHandle, StatsScope};
 use zisk_core::ZiskRom;
 
