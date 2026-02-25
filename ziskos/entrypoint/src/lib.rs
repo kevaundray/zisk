a731e34c64bc82588230b4dee22373f4e20159f9
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2026-02-25 18:28:56 +0000

    revert

--- ziskos/entrypoint/src/lib.rs
+++ ziskos/entrypoint/src/lib.rs
@@ -299,5 +299,7 @@ mod ziskos {
 
     core::arch::global_asm!(include_str!("dma/memcpy.s"));
     core::arch::global_asm!(include_str!("dma/memmove.s"));
-    // core::arch::global_asm!(include_str!("dma/memcmp.s"));
+    core::arch::global_asm!(include_str!("dma/memcmp.s"));
+    //core::arch::global_asm!(include_str!("dma/inputcpy.s"));
+    core::arch::global_asm!(include_str!("dma/memset.s"));
 }
