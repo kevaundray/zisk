984af1e67c21155b871eaea4a6a0855f3b915466
RogerTaule <roger.taulee@gmail.com>
2026-03-12 08:37:40 +0000

    Adding inputs shmem stub

--- emulator-asm/asm-runner/src/lib.rs
+++ emulator-asm/asm-runner/src/lib.rs
@@ -23,7 +23,10 @@ mod hints_file;
 mod hints_shmem;
 #[cfg(not(all(target_os = "linux", target_arch = "x86_64")))]
 mod hints_shmem_stub;
+#[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 mod inputs_shmem;
+#[cfg(not(all(target_os = "linux", target_arch = "x86_64")))]
+mod inputs_shmem_stub;
 #[cfg(all(target_os = "linux", target_arch = "x86_64"))]
 mod multi_shmem;
 mod shmem_reader;
