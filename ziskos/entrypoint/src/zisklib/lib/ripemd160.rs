b9ca06b550df13af77e268d8af15b606828c4a5b
Marcin Bugaj <marcin@lita.foundation>
2026-03-17 18:43:13 +0100

    `ripemd160` C accelerator
    It's implemented purely in software

---
+++ ziskos/entrypoint/src/zisklib/lib/ripemd160.rs
@@ -0,0 +1,24 @@
+/// Compute RIPEMD-160 hash
+#[inline]
+fn ripemd160(input: &[u8]) -> [u8; 32] {
+    use ripemd::Digest;
+    let mut hasher = ripemd::Ripemd160::new();
+    hasher.update(input);
+
+    let mut output = [0u8; 32];
+    hasher.finalize_into((&mut output[12..]).into());
+    output
+}
+
+/// C-compatible wrapper for full RIPEMD160 hash
+///
+/// # Safety
+/// - `input` must point to at least `input_len` bytes
+/// - `output` must point to a writable buffer of at least 32 bytes
+#[cfg_attr(not(feature = "hints"), no_mangle)]
+pub unsafe extern "C" fn ripemd160_c(input: *const u8, input_len: usize, output: *mut u8) {
+    let input_slice = core::slice::from_raw_parts(input, input_len);
+    let hash = ripemd160(input_slice);
+    let output_slice = core::slice::from_raw_parts_mut(output, 32);
+    output_slice.copy_from_slice(&hash);
+}
